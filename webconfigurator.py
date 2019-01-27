bbe8c97f7fb538be8a9df347a17a7c0e4f941162
Bryan Morrison <skeeter_mcbee@live.com>
2019-01-26 18:34:29 -0800

    Initial commit

---
+++ webconfigurator.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    print("Hello world!")
+
+
+
+if __name__ == '__main__':
+    app.run()
