f873d46adea429fa2e899123dc70314914ee900b
Bryan Morrison <skeeter_mcbee@live.com>
2019-01-27 14:17:25 -0800

    update webconfigurator

--- webconfigurator.py
+++ webconfigurator.py
@@ -3,7 +3,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    print("Hello world!")
+    return render_template('index.html')
+
+@app.route('/easymode')
+def easymode():
+    username = request.form['username']
+    robotid = request.form['robotid']
+    cameraid = request.form['cameraid']
+    #_type = request.form['type']
+    streamkey = request.form['streamkey']
+
+    print("%s, %s, %s, %s", username, robotid, cameraid, streamkey)
+    return redirect('/')
 
 
 
