4f3602271d916f2f78a6ec707554f6e97bfb8560
Bryan Morrison <skeeter_mcbee@live.com>
2019-04-05 22:37:10 -0700

    remove sixy mode and login

--- app.py
+++ app.py
@@ -21,13 +21,6 @@ lr_conf_file_dir = local_config.get('configurator', 'lr_conf_file_dir')
 
 robot_config.read(lr_conf_file_dir)
 
-sixy_enabled = local_config.getboolean('sixy_mode', 'enabled')
-sixy_controls = local_config.getboolean('sixy_mode', 'controls')
-chatroom = local_config.get('sixy_mode', 'chatroom')
-sixy_robot = local_config.get('sixy_mode', 'robot')
-sixy_robot_id = local_config.get('sixy_mode', 'robot_id')
-
-
 if __name__ == "__main__":
     app.run(debug=debug_enabled, host="0.0.0.0", port=port)
 
@@ -52,33 +45,8 @@ def options():
                            debug=local_config.get('configurator', 'debug'),
                            port=local_config.get('configurator', 'port'),
                            lr_conf_file_dir=local_config.get(
-                               'configurator', 'lr_conf_file_dir'),
-                           login_enabled=local_config.getboolean(
-                               'login', 'enabled'),
-                           login_username=local_config.get(
-                               'login', 'username'),
-                           login_password=local_config.get(
-                               'login', 'password'),
-                           sixy_enabled=local_config.get(
-                               'sixy_mode', 'enabled'),
-                           sixy_controls=local_config.get(
-                               'sixy_mode', 'controls'),
-                           sixy_robot=local_config.get('sixy_mode', 'robot'),
-                           sixy_robot_id=local_config.get(
-                               'sixy_mode', 'robot_id')
+                               'configurator', 'lr_conf_file_dir')
                            ), 200
-
-
-@app.route('/sixy')
-def sixy():
-    global chatroom
-    global sixy_controls
-    global sixy_enabled
-    global sixy_robot
-    global sixy_robot_id
-    controls = dict(**chatroom, **sixy_controls, **sixy_enabled, **sixy_robot,
-        **sixy_robot_id)
-    return render_template('sixy.html', controls=controls), 200
 
 
 @app.route('/')
@@ -141,24 +109,10 @@ def options_update():
     global debug_enabled
     global port
     global lr_conf_file_dir
-    global login_enabled
-    global login_password
-    global login_username
-    global sixy_enabled
-    global sixy_controls
-    global sixy_robot
-    global sixy_robot_id
 
     debug_enabled = request.form['debug']
     port = request.form['port']
     lr_conf_file_dir = request.form['lr_conf_file_dir']
-    login_enabled = request.form['login_enabled']
-    login_username = request.form['login_username']
-    login_password = request.form['login_password']
-    sixy_enabled = request.form['sixy_enabled']
-    sixy_controls = request.form['sixy_controls']
-    sixy_robot = request.form['sixy_robot']
-    sixy_robot_id = request.form['sixy_robot_id']
 
     return redirect('/options'), 200
 
@@ -168,10 +122,14 @@ def advanced_update():
     global robot_config
     global lr_conf_file_dir
     sections = robot_config.sections()
+
     for section in sections:
         items = dict(robot_config.items(section))
+
         for item in items:
             query = str(section + " " + item)
             result = request.form[query]
             os.system("sed -i '/^\\[%s]/,/^\\[/{s/^%s[[:space:]]*=.*/%s=%s/}' %s" % (
                 section, item, item, result, lr_conf_file_dir))
+
+    return redirect('/advanced'), 200
