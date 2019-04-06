14cee3eb17e30a8a374db72d4873db25777aa831
Bryan Morrison <skeeter_mcbee@live.com>
2019-04-06 16:48:50 -0700

    add 500 error handler

--- app.py
+++ app.py
@@ -79,6 +79,11 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html', e=e), 500
+
+
 @app.route('/api/update/simple', methods=['POST'])
 def simple_update():
     username = request.form['username']
